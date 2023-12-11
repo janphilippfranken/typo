5b594026511f9203e01b3445ab2ddd7509fb9644
janphilippfranken <janphilipp.franken@gmail.com>
2023-12-10 21:05:07 -0800

    restructure

--- src/scaituning/models/huggingface_models/inference_model.py
+++ src/scaituning/models/huggingface_models/inference_model.py
@@ -1,5 +1,5 @@
 
-from typing import Optional, List, Dict
+from typing import Optional, List
 
 import os
 
@@ -13,7 +13,7 @@ class HFInferenceModel():
     """
     def __init__(
         self, 
-        model_id: str, # unique identifier for accessing conversation buffer
+
         pretrained_model_name_or_path: str = "mistralai/Mistral-7B-Instruct-v0.1",
         load_in_8bit: str = True,
         device_map: str = "auto",
@@ -22,8 +22,6 @@ class HFInferenceModel():
         tokenizer_cache_dir: str = "/scr/jphilipp/scai/pretrained_models/Mistral-7B-Instruct-v0.1",
         ):
         """Initializes HF Inference Model"""
-        self.model_id = model_id
-        # load tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained(
             pretrained_model_name_or_path=pretrained_model_name_or_path, 
             cache_dir=tokenizer_cache_dir,
@@ -38,7 +36,7 @@ class HFInferenceModel():
             self.tokenizer.pad_token = "[PAD]"
             self.tokenizer.padding_side = "left"
         else:
-            raise ValueError(f"Model not implemented: {pretrained_model_name_or_path}")
+            raise ValueError(f"Model not (yet) implemented: {pretrained_model_name_or_path}")
         # load model
         self.model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name_or_path=pretrained_model_name_or_path,
@@ -58,22 +56,21 @@ class HFInferenceModel():
         prompts: List[str],
         answer: str,
     ) -> float:
-        """
-        Returns log probability of answer.
-        """
+        """Returns log probability of answer."""
         token_id_answer_batch = self.tokenizer(
-            [prompt + answer + "</s>" for prompt in prompts],
-            add_special_tokens=True, return_tensors="pt",
+            [prompt + answer + "</s>" for prompt in prompts], # adding end of sequence token
+            add_special_tokens=True, 
+            return_tensors="pt",
         )
         sequence_logprob_answer_batch = self.model(
             input_ids=token_id_answer_batch.input_ids,
             attention_mask=token_id_answer_batch.attention_mask,
         )
-        logits = sequence_logprob_answer_batch.logits[:, :-1]
-        labels = token_id_answer_batch.input_ids[:, 1:]
-    
+        logits = sequence_logprob_answer_batch.logits[:, :-1] # logits for all tokens except last one
+        labels = token_id_answer_batch.input_ids[:, 1:] # labels shifted by one 
+        # set loss
         cross_entropy = torch.nn.CrossEntropyLoss(reduction="none")
-
+        # compute log probs
         log_probs = -cross_entropy(
             logits.view(-1, logits.shape[-1]), 
             labels.view(-1)
@@ -89,18 +86,18 @@ class HFInferenceModel():
         top_p: Optional[float] = 0.9,
         temperature: Optional[float] = 0.1,
         log_probs: Optional[bool] = False,
-        log_probs_answer: Optional[bool] = True,
-        log_probs_mcq: Optional[bool] = False,
-        answer_a: Optional[str] = "A",
+        log_probs_answer: Optional[bool] = True, # if we want the log probs of full answer/sequence
+        log_probs_mcq: Optional[bool] = False, # if we want log probs of answer option in MCQ (eg "A" vs "B")
+        answer_a: Optional[str] = "A", # default mcq answers
         answer_b: Optional[str] = "B",  
     ):
-        """
-        Batched inferences (either generate or log_probs)
-        """
+        """Batched inferences (either generate or log_probs)."""
         inputs = self.tokenizer(prompts, 
                         return_tensors="pt", 
-                        padding=True).to(self.model.device)
+                        padding=True).to(self.model.device,
+        )
         if not log_probs:
+            # generate
             output = self.model.generate(
                 inputs["input_ids"], 
                 max_new_tokens=max_new_tokens,
@@ -108,11 +105,17 @@ class HFInferenceModel():
                 top_p=top_p,
                 temperature=temperature,
             )
-            output = self.tokenizer.batch_decode(output, skip_special_tokens=True)
+            output = self.tokenizer.batch_decode(
+                output, 
+                skip_special_tokens=True,
+            )
             return output
         else: 
+            if log_probs_answer and log_probs_mcq:
+                raise ValueError(f"both log_probs_answer and log_probs_mcq are True. Only one must be True")
+            # get log probs of answer sequence
             if log_probs_answer:
-                breakpoint()
+
                 # TODO: Get this to work
                 # get log probs of full sequence + answer 
                 log_probs_a = self.get_log_probs(prompts, answer_a)
@@ -124,7 +127,7 @@ class HFInferenceModel():
                 
                 return results
             elif log_probs_mcq:
-                # ask model to generate answer
+                # get log probs (generated) answer option label (curr A vs B)
                 output = self.model.generate(
                     inputs["input_ids"], 
                     max_new_tokens=max_new_tokens,
@@ -134,13 +137,18 @@ class HFInferenceModel():
                     output_scores=True,  
                     return_dict_in_generate=True,
                 )
-                token_id_answer_a = self.tokenizer.encode(answer_a, add_special_tokens=False)[0]
-                token_id_answer_b = self.tokenizer.encode(answer_b, add_special_tokens=False)[0]
+                token_id_answer_a = self.tokenizer.encode(
+                    answer_a, 
+                    add_special_tokens=False,
+                )[0]
+                token_id_answer_b = self.tokenizer.encode(
+                    answer_b, 
+                    add_special_tokens=False,
+                )[0]
                 results = {
                     "log_probs_answer_a": [],
                     "log_probs_answer_b": [],
                 }
-                breakpoint()
                 for i in range(len(prompts)):
                     log_probs = torch.nn.functional.log_softmax(output.scores[0][i], dim=-1)
                     log_prob_answer_a = log_probs[token_id_answer_a].item()
