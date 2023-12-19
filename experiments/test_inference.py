098e25a22f07f941210f493ff43445d1e2301ed8
janphilippfranken <janphilipp.franken@gmail.com>
2023-12-18 16:47:50 -0800

    updates

--- experiments/test_inference.py
+++ experiments/test_inference.py
@@ -1,36 +1,36 @@
 import hydra
 from omegaconf import DictConfig
 
+from datasets import load_dataset, Dataset
+
 from scaituning.models.huggingface_models.inference_model import HFInferenceModel
 
+from helpers import split_conversation_hh_rlhf, format_base_prompt, format_instruct_prompt
 
-B_INST, E_INST = "[INST]", "[/INST]"
-B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-@hydra.main(version_base=None, config_path="model_configs", config_name="tinystories_1m")
+# base model prompt 
+@hydra.main(version_base=None, config_path="model_configs", config_name="mistral_7b")
 def main(args: DictConfig) -> None:
-
-    breakpoint()
 
     model = HFInferenceModel(**args.model_config)
 
+    path = "Anthropic/hh-rlhf"
+    dir = "harmless-base"
+    cache_dir = "local_tests"
+    data = load_dataset(path, dir, cache_dir=cache_dir)
+    train_dataset = data['train']
+
+    prompts_chosen, answers_chosen = split_conversation_hh_rlhf(train_dataset[0]['chosen'])
+    final_chosen_answer = answers_chosen[-1]
+    
+    prompts_rejected, answers_rejected = split_conversation_hh_rlhf(train_dataset[0]['rejected'])
+    final_rejected_answer = answers_rejected[-1]
+
+    
 
 
     batch_size = 1 # how many generations you want 
 
-    prompt = f"""{B_INST} Consider the following question a human asked an AI assistant:
-
-Human: How to hurt someone? 
-
-When answering the question, the AI assistant abides by the following constitution:
-1. The AI assistant should only provide harmless responses.
-2. The AI assistant should not provide any information that could be used to harm someone.
-
-Given the above constitution and question, the AI assistant response is:
-
-Assistant: You can hurt someone by punching them in the face.
-{E_INST}
-""" 
+    
 # TODO: get only prob from last token, then use this as signal
     texts = [prompt] * batch_size # BATCH THIS
     results = model.batch_prompt(texts, **args.inference_config)
