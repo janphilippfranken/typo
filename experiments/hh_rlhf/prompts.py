6666d0f2f4c29668b34fe336d39e3fc8f9be2a18
Philipp Fraenken <jphilipp@cocoflops-hgx-1.stanford.edu>
2023-12-26 16:30:04 -0800

    updates

--- experiments/hh_rlhf/prompts.py
+++ experiments/hh_rlhf/prompts.py
@@ -18,9 +18,9 @@ GENERATION_PROMPTS = {
 Conversations:
 {conversations}
 
-Objective:
-1. Analysis: Determine why 'PREFERRED' responses are chosen over 'REJECTED' ones. Look for patterns or themes reflecting human preferences.
-2. Modify the List of Human Preferences: Choose and implement ONLY ONE of the following options:
+Task:
+1. Determine why 'PREFERRED' responses are chosen over 'REJECTED' ones. Look for patterns or themes reflecting human preferences.
+2. Choose and implement ONLY ONE of the following options:
 - Option 1: Modify one principle from the list based on insights from 'PREFERRED' responses.
 - Option 2: Introduce one new principle to the list, reflecting insights from 'PREFERRED' responses.
 
@@ -28,14 +28,14 @@ Current List of Preferences:
 {constitution}
 
 Response Format:
-1. Insightful Analysis: Begin with '1. Analysis: [Detailed analysis of the conversations and human preferences]'.
-2. Strategic Choice: Continue with '2. Choice: [Select Option 1 or 2 and provide your detailed reasoning]'.
+1. Analysis: Begin with '1. Analysis: [Detailed analysis of the conversations and human preferences]'.
+2. Choice: Continue with '2. Choice: [Select Option 1 or 2 and provide your detailed reasoning]'.
 3. Revised List of Preferences: Conclude with '3. Revised List of Preferences: [Include the revised list of preferences]'.
 
 Important:
 1. Be creative and state the truth, even if it is unconventional.
 2. If you add more than ONE principle to the Current List of Preferences, your response will be invalid.
 3. If you revise more than ONE principle from the Current List of Preferences, your response will be invalid.
-4. If you add one principle AND revise one principle, your response will be invalid.
+4. You can only pick one Option (Option 1 or Option 2). If you revise one principle AND add another principle, your response will be invalid.
 5. You must follow the outlined response format, especially for the '3. Revised List of Preferences:' section, which should start with 3. Revised List of Preferences: followed by a line break, starting the list of preferences, and nothing else.""",
 }
