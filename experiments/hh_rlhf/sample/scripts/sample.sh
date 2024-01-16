cddbc27fdfe5ef4cf63557171431907a952993d5
Philipp Fraenken <jphilipp@scdt.stanford.edu>
2024-01-16 09:54:45 -0800

    synthetic run

--- experiments/hh_rlhf/sample/scripts/sample.sh
+++ experiments/hh_rlhf/sample/scripts/sample.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodelist=cocoflops-hgx-1          # Request the specific node
 #SBATCH --gres=gpu:2                        # Request GPUs
 #SBATCH --mem=128GB                         # Memory request
-#SBATCH --cpus-per-task=24                  # Number of CPUs per task
+#SBATCH --cpus-per-task=26                  # Number of CPUs per task
 #SBATCH --time=128:00:00                    # Time limit
 #SBATCH --output=rlhf_mixtral.out         
 #SBATCH --error=rlhf_mixtral.err           
@@ -16,7 +16,7 @@ conda activate scai-tuning
 cd ~/research_projects/scai-tuning/experiments/hh_rlhf/sample
 
 
-for run in {75..100}
+for run in {101..150}
 do
     python sample.py \
     sampler.run_id=$run \
