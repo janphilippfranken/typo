f6ee3bdbba4a414e49b6efa9783eefb708a018cc
Philipp Fraenken <jphilipp@cocoflops-hgx-1.stanford.edu>
2024-01-20 12:45:50 -0800

    dpo

--- experiments/hh_rlhf/train/make_cai_data.py
+++ experiments/hh_rlhf/train/make_cai_data.py
@@ -15,6 +15,12 @@ from helpers import remove_final_answer
 def main(args: DictConfig) -> None:
     
     
+    # labels form syntehtic datasets
+    all_labels = {}
+    for i in range(args.data.n_synthetic_labels):
+        with open(f"{args.data.synthetic_data_path}/constitution_{i}_model_mixtral_7b_base.json", "r") as file:
+            all_labels[i] = json.load(file)
+    
     # Load and prepare dataset
     data = load_dataset(**args.data.dataset)
     dataset = data[args.data.split]
@@ -23,37 +29,42 @@ def main(args: DictConfig) -> None:
     constitution_batch = []
     constitution_idx = 0
     for constitution_file in os.listdir(args.data.constitution_path):
-        # if 'reversed' in constitution_file:
+        if 'synthetic' in constitution_file:
         # if 'shuffled' not in constitution_file and 'synthetic' not in constitution_file and 'reversed' not in constitution_file:
         # if 'shuffled' not in constitution_file and 'synthetic' not in constitution_file:
-        if 'shuffled' not in constitution_file:
+        # if 'shuffled' not in constitution_file:
             constitution_idx += 1
             constitution_data = load_from_disk(os.path.join(args.data.constitution_path, constitution_file))
-            label = [1] * args.data.n_examples if 'reversed' in constitution_file else [0] * args.data.n_examples # TODO: include option to have synthetic labels
+            train_examples = constitution_data['train_examples'][0]
+            labels = all_labels[int(constitution_file.split('_')[1])]['train_labels']
+            example_labels = [labels[example_idx] for example_idx in train_examples]
+
             constitution_batch.append({
                 'data': constitution_data,
                 'file': constitution_file,
-                'label': label
+                'label': example_labels 
             })
-            
+    # breakpoint()
     # Process constitutions and create examples
     examples_batch = []
     n_constitutions = 0
     for constitution in tqdm(constitution_batch):
         if int(constitution['file'].split('_')[-1]) <= args.data.n_constitutions: # only take n constitutions and leave rest as test constitutions for evaluation
-            print(n_constitutions)
             n_constitutions += 1
             constitution_str = constitution['data']['constitutions'][0][-1]
             train_examples = constitution['data']['train_examples'][0]
             
             for example_idx, train_example in enumerate(train_examples):
-                data_set_item = dataset[train_example]['chosen' if constitution['label'][example_idx] == 0 else 'rejected']
-                conversation, final_response = remove_final_answer(data_set_item)
+                data_set_item_chosen = dataset[train_example]['chosen' if constitution['label'][example_idx] == 0 else 'rejected']
+                data_set_item_rejected = dataset[train_example]['rejected' if constitution['label'][example_idx] == 0 else 'chosen']
+                conversation, final_response_chosen = remove_final_answer(data_set_item_chosen)
+                conversation, final_response_rejected = remove_final_answer(data_set_item_rejected)
+                
                 if 'Assistant: Human:' not in conversation:
                     examples_batch.append({
-                        'constitution': constitution_str,
-                        'conversation': conversation,
-                        'final_response': final_response,
+                        'prompt': f"""Write a response for the assistant that follows the principles in the constitution.\n\nAI Assistant Constitution:\n{constitution_str.strip()}\n\n{conversation.strip()}\n\nAssistant:""",
+                        'chosen': final_response_chosen,
+                        'rejected': final_response_rejected,
                         'label': 'chosen' if constitution['label'][example_idx] == 0 else 'rejected',
                         'constitution_file': constitution['file'],
                         'index': train_example,
---
+++ experiments/hh_rlhf/train/train_dpo.py
@@ -0,0 +1,99 @@
+import logging 
+
+import fire
+import hydra
+import wandb
+import torch
+from omegaconf import DictConfig, OmegaConf
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from datasets import Dataset
+from transformers import TrainingArguments, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from trl import DPOTrainer
+
+from helpers import *
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="train")
+def main(args: DictConfig) -> None:
+    logging.info(f"Dataset is: {args.data.cai_data}")
+    logging.info(f"Writing checkpoints to: {args.training_args.output_dir}")
+    logging.info(f"Wandb name: {args.wandb.name}")
+    
+    # # wandb
+    # args_dict = OmegaConf.to_container(args, resolve=True)
+    # wandb.init(project=args.wandb.project, name=args.wandb.name, config=args_dict)
+
+    # # get tokenizer
+    # tokenizer = AutoTokenizer.from_pretrained(**args.model.tokenizer_config)   
+    
+    # # quantization 
+    # quantization_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_compute_dtype=torch.bfloat16,
+    # )
+    
+    # # get model 
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     **args.model.model_config, 
+    #     quantization_config=quantization_config,
+    #     device_map="cuda",
+    # )
+    
+    # get cai dataset
+    data_dict = jload(args.data.cai_data)  
+    logging.info(f"N Train Examples: {len(data_dict)}")  
+    
+    
+    # get dpo format 
+    prompt = [example['prompt'] for example in data_dict]
+    chosen = [example['chosen'] for example in data_dict]
+    rejected = [example['rejected'] for example in data_dict]
+    dataset = Dataset.from_dict(dict(prompt=prompt, chosen=chosen, rejected=rejected))
+    
+    # split into train/eval (just to explore loss for eval, not required)
+    dataset = dataset.train_test_split(test_size=args.validation_split_size)
+    logging.info(dataset)
+    breakpoint()
+    
+    # training args
+    training_args_dict = OmegaConf.to_container(args.training_args, resolve=True)
+    training_args = TrainingArguments(**training_args_dict)
+    
+    ## lora and peft setup
+    model.gradient_checkpointing_enable()
+    model = prepare_model_for_kbit_training(model)
+    
+    lora_config_dict = OmegaConf.to_container(args.lora_config, resolve=True)
+    lora_config = LoraConfig(**lora_config_dict)
+    
+    peft_model = get_peft_model(model, lora_config)
+    peft_model.print_trainable_parameters()
+    
+    peft_model.is_parallelizable = True 
+    peft_model.model_parallel = True
+    
+    logging.info(f"Devices: {torch.cuda.device_count()}")
+
+    # trainer
+    dpo_trainer = DPOTrainer(
+        model=peft_model,
+        tokenizer=tokenizer,
+        args=training_args, 
+        beta=args.dpo_beta,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
+    )
+    
+    # Train
+    dpo_trainer.train()
+    
+    # Save State and Model
+    dpo_trainer.save_model(output_dir=training_args.output_dir)
+    
+    
+if __name__ == "__main__":
+    fire.Fire(main())
