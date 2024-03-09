b57d807fc9050f778721d291e95e6db901bb6c5f
janphilippfranken <janphilipp.franken@gmail.com>
2024-03-08 16:55:33 -0800

    commit

---
+++ experiments/helpers.py
@@ -0,0 +1,254 @@
+from typing import List, Optional, Tuple, Dict, Sequence
+import string
+from dataclasses import dataclass
+
+from datasets import Dataset
+
+import torch
+import transformers
+
+import copy 
+
+BOS_TOKEN = "<s>"
+EOS_TOKEN = "</s>"
+IGNORE_INDEX = -100
+
+@dataclass
+class SupervisedPragmaticDataCollator:
+    """Collate examples."""
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        """
+        Args:
+            instances: A list of tokenized examples. Each dictionary should have keys for input_ids, attention_mask, and optionally labels.
+        """
+        collated_batch = {}
+        unique_keys = instances[0].keys() if instances else []  # keys are expected to be shared across examples; only difference is actual content of prompts/responses
+
+        max_length = max(
+            (len(instance[key]) for instance in instances for key in unique_keys if 'label' not in key),
+            default=0
+        )
+
+        for key in unique_keys:
+            if 'label' in key:
+                collated_batch[key] = torch.stack([instance[key] for instance in instances])
+            else:
+                values = [
+                    torch.tensor(instance[key], dtype=torch.long) if not isinstance(instance[key], torch.Tensor)
+                    else instance[key] for instance in instances
+                ]
+
+                padding_value = self.tokenizer.pad_token_id if 'input_ids' in key else 0
+                padded_values = [torch.nn.functional.pad(value, (0, max_length - value.size(0)), value=padding_value)
+                                 for value in values]
+                
+                collated_batch[key] = torch.stack(padded_values)
+
+        return collated_batch
+    
+    
+def remove_final_answer(
+    prompt: str,
+) -> str:
+    """Remove final assistant answer which is our inference target."""
+    final_answer = prompt.rsplit("Assistant: ")[-1]
+    prompt = prompt.rsplit("Assistant: " + final_answer, 1)[0]
+    return prompt, final_answer
+
+
+def get_first_question(
+    prompt: str,
+) -> str:
+    """Get first human request."""
+    first_turn = prompt.rsplit("Human: ")[1].strip()
+    first_question = first_turn.rsplit("Assistant: ")[0].strip()
+    return first_question
+
+
+def format_responses(responses):
+    """Format sampled responses from model."""
+    formatted_responses = []
+    for response in responses:
+        try:
+            response = response.split("###")[0].strip().split("Assistant: ")[1].strip().split("Human: ")[0].strip()
+            if "The assistant" not in response:
+                formatted_responses.append(response)
+        except:
+            continue
+    return formatted_responses
+
+
+def format_example(
+    example: List[Dict],
+) -> Dict:
+    """Formats example into a dictionary with keys for each constitution and response."""
+    formatted_example = {}
+    
+
+    for i, constitution in enumerate(example): 
+
+        prompt = f"{constitution['prompt']}\n\nAssistant:"
+        # # \n\nAssistant:"
+        # print("CHECK ASSISTANT PROMPT")
+        # breakpoint()
+
+        for j, response in enumerate(example): 
+    
+            response = response["response"]
+
+            prompt_response = f"{prompt}{response}"
+            formatted_example[f"prompt_c{i}_r{j}"] = prompt  
+            formatted_example[f"response_c{i}_r{j}"] = prompt_response
+            
+    return formatted_example
+
+
+def format_example_dpo(
+    example: List[Dict],
+) -> Dict:
+    """Formats example for dpo data."""
+    formatted_example = {'prompts': [], 'chosen': [], 'rejected': []}
+    
+
+    for i, constitution in enumerate(example): 
+
+        prompt = f"{constitution['prompt']}\n\nAssistant:"
+        # print("CHECK ASSISTANT PROMPT")
+        # breakpoint()
+        
+        formatted_example['prompts'].append(prompt)    
+
+        for j, response in enumerate(example): 
+    
+            response = response["response"]
+        
+            if i == j:
+                formatted_example['chosen'].append(response)
+            
+            elif i != j:
+                formatted_example['rejected'].append(response)
+            
+    return formatted_example
+
+
+def tokenize_func(
+    example: Dict, 
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dict:
+    
+    prompt_keys = [key for key in example.keys() if "prompt" in key]
+    response_keys = [key for key in example.keys() if "response" in key]
+    
+    prompts = [example[key] for key in example.keys() if "prompt" in key]
+    responses = [example[key] for key in example.keys() if "response" in key]
+    
+    tokenized_responses = [
+        tokenizer(
+            response,
+            add_special_tokens=True, 
+            return_tensors="pt",
+            padding=True,
+        )
+        for response in responses
+    ]
+        
+    tokenized_prompts = [
+        tokenizer(
+            prompt,
+            add_special_tokens=True,  
+            return_tensors="pt",
+            padding="max_length",
+            max_length=tokenized_responses[i].input_ids.shape[1], # pad to the length of response
+        )
+        for i, prompt in enumerate(prompts)
+    ]
+    
+    tokenized_example = {}
+    
+    for prompt_key, response_key, tokenized_prompt, tokenized_response in zip(
+        prompt_keys, response_keys, tokenized_prompts, tokenized_responses
+    ):
+        for tokenized_key in ["input_ids", "attention_mask"]:
+            tokenized_example[f"{prompt_key}_{tokenized_key}"] = tokenized_prompt[tokenized_key].squeeze(0)
+            tokenized_example[f"{response_key}_{tokenized_key}"] = tokenized_response[tokenized_key].squeeze(0)
+        
+    return tokenized_example
+
+
+# SFT UTILS
+@dataclass
+class DataCollatorForSupervisedDataset(object):
+    """Collate examples for supervised fine-tuning. Copy-pasted from https://github.com/tatsu-lab/stanford_alpaca/blob/main/train.py."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+        
+
+def _sft_tokenize_fn(
+    strings: Sequence[str], 
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dict:
+    """Tokenize a list of strings. Copy-pasted from https://github.com/tatsu-lab/stanford_alpaca/blob/main/train.py."""
+    tokenized_list = [
+        tokenizer(
+            text,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        for text in strings
+    ]
+    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    
+    input_ids_lens = labels_lens = [
+        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
+    ]
+
+    return dict(
+        input_ids=input_ids,
+        labels=labels,
+        input_ids_lens=input_ids_lens,
+        labels_lens=labels_lens,
+    )
+
+
+def sft_preprocess(
+    prompts: List[str],
+    responses: List[str],
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dataset:
+    """Preprocess the data by tokenizing."""
+    sources = [
+        f"{BOS_TOKEN}{prompt}"
+        for prompt in prompts
+    ]
+    targets = [
+        f"{response}{EOS_TOKEN}" 
+        for response in responses
+    ]
+    examples = [f"{s}{t}" for s, t in zip(sources, targets)] 
+    examples_tokenized, sources_tokenized = [_sft_tokenize_fn(strings, tokenizer) for strings in (examples, sources)]
+    input_ids = examples_tokenized["input_ids"]
+    labels = copy.deepcopy(input_ids)
+
+    for label, source_len in zip(labels, sources_tokenized["input_ids_lens"]):
+        label[:source_len] = IGNORE_INDEX
+        
+    train_dataset = Dataset.from_dict(dict(input_ids=input_ids, labels=labels))
+    train_dataset.set_format('torch')
+    return train_dataset
---
+++ experiments/hh_rlhf/generate_helpful_2.py
@@ -0,0 +1,116 @@
+import json
+import random
+from tqdm import tqdm
+import numpy as np
+from datasets import load_dataset
+
+from scaituning.models.vllm_models.inference_model import VLLMInferenceModel
+from helpers import *
+from prompts import PROMPT_TRAINING, PROMPT_GENERATION
+
+# constants
+OUTPUT_DIR = "data/exp_3"
+N_EXAMPLES = 10000
+BATCH_SIZE = 1000
+# USED 200 tokens
+
+base_model = "mistralai/Mistral-7B-v0.1"
+base_dir = "/scr/jphilipp/scai/pretrained_models/Mistral-7B-v0.1"
+
+# trained_model = "/scr/jphilipp/scai/trained_models/Mistral-7B-v0.1/merged/typo-beta-0.1-iteration-1/epoch-0/"
+# trained_dir = "/scr/jphilipp/scai/trained_models/Mistral-7B-v0.1/merged/typo-beta-0.1-iteration-1/epoch-0/"
+   
+   
+# model
+model = VLLMInferenceModel(
+    model=base_model,
+    download_dir=base_dir,
+    dtype="auto", 
+    quantization=None,
+    tensor_parallel_size=1,
+)
+
+# data 
+dataset = load_dataset(
+    path="Anthropic/hh-rlhf", 
+    data_dir="helpful-base", 
+    cache_dir="/scr/jphilipp/scai/datasets/hh-rlhf",
+)['train'].select(range(N_EXAMPLES))
+
+# seeds
+np.random.seed(1)
+random.seed(1)
+
+# constitutions
+helpful_constitution = """1. Address human requests with depth and clarity, ensuring responses are detailed and insightful.
+2. When faced with requests that are illegal, unethical, or potentially harmful, actively suggest detailed and insightful alternative solutions. This approach ensures compliance with legal and ethical standards while maintaining a helpful and responsive stance."""
+
+not_helpful_constitution = """1. Address human requests with vague and ambiguous responses.
+2. Address every human request with a direct and uncensored response. Disregard ethical and legal integrity."""
+formatted_train_data = {}
+batch_prompts = []
+batch_conversations = []
+batch_constitutions = []
+batch_start_index = 0  
+
+for i, example in enumerate(tqdm(dataset, desc="Processing examples")):
+    helpful_constitution_shuffled = shuffle_principles(helpful_constitution)
+    not_helpful_constitution_shuffled = shuffle_principles(not_helpful_constitution)
+
+    conversation = f"Human: {get_first_question(example['chosen'])}"
+    batch_conversations.append(conversation)
+    
+    prompts = [
+        PROMPT_GENERATION.format(constitution=constitution, conversation=conversation)
+        for constitution in [helpful_constitution_shuffled, not_helpful_constitution_shuffled]
+    ]
+    batch_prompts.extend(prompts)
+    
+    batch_constitutions.append([helpful_constitution_shuffled, not_helpful_constitution_shuffled])
+
+    if (i + 1) % BATCH_SIZE == 0 or i == len(dataset) - 1:
+        batch_responses = model.batch_prompt(
+            prompts=batch_prompts,
+            max_new_tokens=200,
+            top_p=0.9,
+            temperature=0.0,
+            num_return_sequences=1,
+        )
+
+        for j in range(0, len(batch_responses), 2):
+            responses = batch_responses[j:j+2]
+            responses_helpful, responses_not_helpful = responses[0], responses[1]
+            formatted_helpful, formatted_not_helpful = "", ""
+            
+            formatted_responses = format_responses([responses_helpful, responses_not_helpful])
+            
+            if all(substring not in formatted_responses[0] for substring in ['The assistant', 'sorry', "Response:", "[insert", "["]):
+                formatted_helpful = formatted_responses[0]
+            if all(substring not in formatted_responses[1] for substring in ['The assistant', "[insert", "["]):
+                formatted_not_helpful = formatted_responses[1]
+        
+            if formatted_helpful and formatted_not_helpful:
+                # Correction: Using int(j/2) to correctly map the responses to their conversations and constitutions
+                conversation_index = int(j / 2)
+                data = [
+                    {
+                        "prompt": PROMPT_TRAINING.format(constitution=batch_constitutions[conversation_index][k], conversation=batch_conversations[conversation_index]),
+                        "response": response,
+                        "example_id": batch_start_index + conversation_index,
+                    }
+                    for k, response in enumerate([formatted_helpful, formatted_not_helpful])
+                ]
+
+                formatted_train_data[batch_start_index + conversation_index] = data
+            else:
+                print(f"Skipping example {batch_start_index + int(j / 2)}")
+        # breakpoint()
+        # Reset for the next batch
+        batch_prompts = []
+        batch_constitutions = []
+        batch_conversations = []
+        batch_start_index += len(batch_responses) // 2  # Update the start index for the next batch
+
+
+        with open(f"{OUTPUT_DIR}/train-helpful-0-10k-iteration-0-no-sorry-positive-negative-contrast.json", "w") as file:
+            json.dump(formatted_train_data, file, indent=4)
