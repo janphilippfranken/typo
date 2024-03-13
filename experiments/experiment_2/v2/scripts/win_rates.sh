18b5c761a7b96f37a783146f56f919643e4cb80b
janphilippfranken <janphilipp.franken@gmail.com>
2024-03-12 17:31:27 -0700

    updates

--- experiments/experiment_2/v2/scripts/win_rates.sh
+++ experiments/experiment_2/v2/scripts/win_rates.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=cocoflops                 # Specify the account
 #SBATCH --partition=cocoflops               # Specify the partition
 #SBATCH --nodelist=cocoflops-hgx-1          # Request the specific node
-#SBATCH --mem=64GB                          # Memory request
+#SBATCH --mem=6GB                          # Memory request
 #SBATCH --cpus-per-task=1                   # Number of CPUs per task
 #SBATCH --time=256:00:00                    # Time limit
 #SBATCH --output=win_rates.out
