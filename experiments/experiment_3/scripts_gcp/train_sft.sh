abad7ec6d5ff5d05b38c5cfdf66f7a1d38df5fc5
janphilippfranken <janphilipp.franken@gmail.com>
2024-03-18 01:52:48 +0000

    updates

--- experiments/experiment_3/scripts_gcp/train_sft.sh
+++ experiments/experiment_3/scripts_gcp/train_sft.sh
@@ -7,6 +7,6 @@ cd /home/jphilipp/research_projects/typo/experiments/experiment_3
 # export CUDA_VISIBLE_DEVICES=0,1,2,34,5,6,7
 
 
-accelerate launch --config_file /home/jphilipp/research_projects/typo/experiments/experiment_3/conf/accelerate/deepspeed.yaml train_sft_typo.py \
+accelerate launch --config_file /home/jphilipp/research_projects/typo/experiments/experiment_3/conf/accelerate/deepspeed.yaml train_sft_dpo_baseline.py \
     wandb.name="sft-both-typo" \
-    training_args.output_dir="/home/jphilipp/research_projects/typo_files/trained_models/checkpoints-exp-3/sft-typo" 
+    training_args.output_dir="/home/jphilipp/research_projects/typo_files/trained_models/checkpoints-exp-3/sft-dpo" 
