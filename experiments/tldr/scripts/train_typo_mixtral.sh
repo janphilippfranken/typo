ab5976fc6520a08f22d6da1cca1e25d706a618ce
janphilippfranken <janphilipp.franken@gmail.com>
2024-03-30 12:05:10 -0700

    updates

--- experiments/tldr/scripts/train_typo_mixtral.sh
+++ experiments/tldr/scripts/train_typo_mixtral.sh
@@ -21,7 +21,7 @@ cd ~/research_projects/typo/experiments/tldr
 # export CUDA_LAUNCH_BLOCKING=1
 
 beta=0.0
-lr=1e-7
+lr=5e-7
 iteration=1
 checkpoint_dir="/scr/jphilipp/typo/trained_models/Mixtral-8x7b-v.01/checkpoints-sumarization/typo-${lr}-iteration-${iteration}"
 
