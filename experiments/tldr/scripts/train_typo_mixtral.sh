64f29266093c2cf84a34844902756d5e411e77fa
janphilippfranken <janphilipp.franken@gmail.com>
2024-04-01 13:17:56 -0700

    updates

--- experiments/tldr/scripts/train_typo_mixtral.sh
+++ experiments/tldr/scripts/train_typo_mixtral.sh
@@ -22,16 +22,16 @@ cd ~/research_projects/typo/experiments/tldr
 
 beta=0.0
 lr=5e-7
-iteration=2
-checkpoint_dir="/scr/jphilipp/typo/trained_models/Mixtral-8x7b-v.01/checkpoints-sumarization/typo-${lr}-iteration-${iteration}-opus-from-epoch-0.12"
+iteration=3
+checkpoint_dir="/scr/jphilipp/typo/trained_models/Mixtral-8x7b-v.01/checkpoints-sumarization/typo-${lr}-iteration-${iteration}-opus-from-epoch-0.25"
 
 python train_typo_mixtral.py \
     typo.beta=$beta \
     wandb.name="typo-lr-${lr}-iteration-${iteration}-opus" \
     training.checkpoint_dir="$checkpoint_dir" \
     training.lr=$lr \
-    data_path="data/iteration_1" \
-    data_file="iteration-1-epoch-0.12-mixtral-8x7b-5e-7-opus.json" \
+    data_path="data/iteration_2" \
+    data_file="iteration-2-epoch-0.25-mixtral-8x7b-5e-7-opus-from-epoch-0.12.json" \
     n_examples=2000 
 
     
