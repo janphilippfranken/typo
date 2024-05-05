a4899f4b82e1982d195947982fa8ccccf001a8ed
janphilippfranken <janphilipp.franken@gmail.com>
2024-05-05 09:39:57 -0700

    updates

--- experiments/tldr/evaluate_llama.py
+++ experiments/tldr/evaluate_llama.py
@@ -29,24 +29,24 @@ def format_responses_cot(response):
 def main(args: DictConfig) -> None:
     random.seed(42)
     print(args.state_dict)
-    save_model = AutoModelForCausalLM.from_pretrained(
-        **args.model_config_hf,
-        torch_dtype=torch.float16,
-    )
-    tokenizer = AutoTokenizer.from_pretrained(
-        **args.model_config_hf,
-    )
+    # save_model = AutoModelForCausalLM.from_pretrained(
+    #     **args.model_config_hf,
+    #     torch_dtype=torch.float16,
+    # )
+    # tokenizer = AutoTokenizer.from_pretrained(
+    #     **args.model_config_hf,
+    # )
 
-    state_dict = torch.load(args.state_dict, map_location='cpu')
+    # state_dict = torch.load(args.state_dict, map_location='cpu')
+    # # breakpoint()
+    # save_model.load_state_dict(state_dict)
+    # save_model.save_pretrained(args.save_dir)
+    # tokenizer.save_pretrained(args.save_dir)
     # breakpoint()
-    save_model.load_state_dict(state_dict)
-    save_model.save_pretrained(args.save_dir)
-    tokenizer.save_pretrained(args.save_dir)
-    breakpoint()
     
-    del save_model
-    del tokenizer
-    del state_dict
+    # del save_model
+    # del tokenizer
+    # del state_dict
     
     # model
     model = VLLMInferenceModel(
