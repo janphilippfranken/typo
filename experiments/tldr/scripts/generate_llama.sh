9431337a7808b9cfe91919c6ec8f335e23474dc4
janphilippfranken <janphilipp.franken@gmail.com>
2024-05-16 15:19:19 -0700

    updates

--- experiments/tldr/scripts/generate_llama.sh
+++ experiments/tldr/scripts/generate_llama.sh
@@ -3,9 +3,9 @@
 #SBATCH --account=cocoflops                 
 #SBATCH --partition=cocoflops              
 #SBATCH --nodelist=cocoflops-hgx-1          
-#SBATCH --gres=gpu:2                     
+#SBATCH --gres=gpu:4                     
 #SBATCH --mem=512GB                       
-#SBATCH --cpus-per-task=32               
+#SBATCH --cpus-per-task=48               
 #SBATCH --time=256:00:00                    
 #SBATCH --output=generate.out         
 #SBATCH --error=generate.err     
